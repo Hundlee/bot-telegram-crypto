5e70864a29567cf140218cebeff84216058d048e
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-03-25 16:14:57 -0300

    feat: Add function to send the prices of the 10 cryptocurrencies with the lowest values

--- src/bot/actions.ts
+++ src/bot/actions.ts
@@ -11,6 +11,39 @@ export async function sendPriceHighCoins(ctx: any) {
     const HighCoins = data.slice(0, 10);
 
     HighCoins.forEach((coin: any) => {
-        return ctx.reply(`${coin.name} : ${coin.quote.USD.price.toFixed(2)}`);
+        return ctx.reply(
+            `${coin.name} : ${coin.quote.USD.price.toFixed(
+                2
+            )} ${coin.quote.USD.percent_change_1h.toFixed(2)}% na ultima hora `
+        );
+    });
+}
+
+export async function sendLowestPriceCoins(ctx: any) {
+    const response = await getApi();
+    const data = response.data;
+
+    const LowPriceCoins = data.slice(0, 10);
+
+    LowPriceCoins.sort((a: any, b: any) => {
+        return a.quote.USD.price - b.quote.USD.price;
+    });
+
+    LowPriceCoins.forEach((coin: any) => {
+        let priceFormatted;
+
+        if (coin.quote.USD.price > 1) {
+            priceFormatted = coin.quote.USD.price.toFixed(2);
+        } else {
+            priceFormatted = coin.quote.USD.price.toFixed(6);
+        }
+
+        return ctx.reply(
+            `${
+                coin.name
+            } : $${priceFormatted} ${coin.quote.USD.percent_change_1h.toFixed(
+                2
+            )}% na ultima hora `
+        );
     });
 }
