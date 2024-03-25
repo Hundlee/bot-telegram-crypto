26340dc35474193c6def4432bf5c03bbe76f613f
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-03-25 16:17:52 -0300

    feat: Add function to send cryptocurrencies that have grown in the last 24 hours

--- src/bot/actions.ts
+++ src/bot/actions.ts
@@ -78,3 +78,34 @@ export async function sendCoinsGrowingLastHour(ctx: any) {
         );
     });
 }
+
+export async function sendCoinsGrowingLastDay(ctx: any) {
+    const response = await getApi();
+    const data = response.data;
+
+    const coinsGrowingLastHour = data.filter(
+        (coin: any) => coin.quote.USD.percent_change_24h > 0
+    );
+
+    coinsGrowingLastHour.sort((a: any, b: any) => {
+        return b.quote.USD.percent_change_24h - a.quote.USD.percent_change_24h;
+    });
+
+    coinsGrowingLastHour.forEach((coin: any) => {
+        let priceFormatted;
+
+        if (coin.quote.USD.price > 1) {
+            priceFormatted = coin.quote.USD.price.toFixed(2);
+        } else {
+            priceFormatted = coin.quote.USD.price.toFixed(6);
+        }
+
+        return ctx.reply(
+            `${
+                coin.name
+            } : ${priceFormatted} cresceu ${coin.quote.USD.percent_change_24h.toFixed(
+                2
+            )}% no ultimo dia`
+        );
+    });
+}
