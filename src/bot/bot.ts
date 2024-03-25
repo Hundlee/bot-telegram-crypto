5c7ffe61731452c94f4100635b08641c92eb1d4e
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-03-25 16:18:37 -0300

    feat: added commands

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,48 +1,31 @@
 import { Bot } from "grammy";
-const axios = require("axios");
 require("dotenv").config();
+import {
+    sendPriceHighCoins,
+    sendLowestPriceCoins,
+    sendCoinsGrowingLastHour,
+    sendCoinsGrowingLastDay,
+} from "./actions";
 
 const botToken = process.env.BOT_TOKEN;
 
 const bot = new Bot(botToken as string);
 
-async function getApi() {
-    try {
-        const response = await axios.get("http://localhost:8080/");
-        return response.data;
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-async function sendPriceTopCoins(ctx: any) {
-    const response = await getApi();
-    const data = response.data;
-
-    data.sort((a: any, b: any) => {
-        return b.quote.USD.price - a.quote.USD.price;
-    });
-
-    const topCoins = data.slice(0, 10);
-
-    topCoins.forEach((coin: any) => {
-        return ctx.reply(`${coin.name} : ${coin.quote.USD.price.toFixed(2)}`);
-    });
-}
-
 bot.command("start", (ctx) => ctx.reply("Hello!"));
 
-bot.command("top", sendPriceTopCoins);
+bot.command("highprice", sendPriceHighCoins);
 
-bot.hears("cego é?", async (ctx) => {
-    // `reply` is an alias for `sendMessage` in the same chat (see next section).
-    await ctx.reply("gay", {
-        // `reply_parameters` specifies the actual reply feature.
+bot.command("lowprice", sendLowestPriceCoins);
+
+bot.command("lasthour", sendCoinsGrowingLastHour);
+
+bot.command("lastday", sendCoinsGrowingLastDay);
+
+bot.hears("tudo bem", async (ctx) => {
+    await ctx.reply("sim", {
         reply_parameters: { message_id: ctx.msg.message_id },
     });
 });
-
-bot.hears("marlon", (ctx) => ctx.reply("esse é esquisito"));
 
 bot.on("message", async (ctx) => {
     // Get the chat identifier.
